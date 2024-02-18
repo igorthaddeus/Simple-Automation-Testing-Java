971af08666127cff66c314868377bccc37114044
igorthaddeus <igorthaddeus@yahoo.com>
2024-02-18 13:55:11 +0700

    Automation testing

--- src/test/java/Selenium/RegisterTest.java
+++ src/test/java/Selenium/RegisterTest.java
@@ -24,4 +24,71 @@ public class RegisterTest extends BaseTest{
                 driver.findElement(By.xpath("//h2[contains(text(), 'Pilih Metode Verifikasi')]")).getText(),
                 "Pilih Metode Verifikasi");
     }
+
+    @Test
+    public void blankFormRegister(){
+        RegisterPage rp = landingPage.goToRegisterPage();
+        rp.goToRegisterPage();
+        Boolean button = driver.findElement(By.cssSelector("button[data-testid='button-submit']")).isEnabled();
+        Assert.assertFalse(button);
+    }
+
+    @Test
+    public void invalidEmail(){
+        landingPage.goTo();
+        RegisterPage rp = landingPage.goToRegisterPage();
+        rp.goToRegisterPage();
+        rp.clickCardRegister();
+        rp.setFullnameRegister(fullname);
+        rp.setEmailRegister(invalidEmail);
+        rp.clickCheckboxRegister();
+
+        Assert.assertEquals(
+                driver.findElement(By.xpath("//p[normalize-space()='Format Email atau Nomor Ponsel salah']")).getText(),
+                "Format Email atau Nomor Ponsel salah"
+        );
+        Boolean button = driver.findElement(By.cssSelector("button[data-testid='button-submit']")).isEnabled();
+        Assert.assertFalse(button);
+    }
+
+    @Test
+    public void invalidPassword() throws InterruptedException {
+        landingPage.goTo();
+        RegisterPage rp = landingPage.goToRegisterPage();
+        rp.goToRegisterPage();
+        rp.clickCardRegister();
+        rp.setFullnameRegister(fullname);
+        rp.setEmailRegister(email);
+        Thread.sleep(2000);
+        rp.setPasswordRegister(invalidPassword);
+        rp.clickCheckboxRegister();
+
+        Assert.assertEquals(
+                driver.findElement(By.xpath("//p[contains(text(),'Min. 8 - 50 karakter terdiri dari huruf kecil besa')]")).getText(),
+                "Min. 8 - 50 karakter terdiri dari huruf kecil besar, angka, dan simbol"
+        );
+        Boolean button = driver.findElement(By.cssSelector("button[data-testid='button-submit']")).isEnabled();
+        Assert.assertFalse(button);
+    }
+
+    @Test
+    public void passwordNotMatch() throws InterruptedException {
+        landingPage.goTo();
+        RegisterPage rp = landingPage.goToRegisterPage();
+        rp.goToRegisterPage();
+        rp.clickCardRegister();
+        rp.setFullnameRegister(fullname);
+        rp.setEmailRegister(email);
+        Thread.sleep(2000);
+        rp.setPasswordRegister(password);
+        rp.setConfirmPasswordRegister("password");
+        rp.clickCheckboxRegister();
+
+        Assert.assertEquals(
+                driver.findElement(By.xpath("//p[normalize-space()='Kolom ulangi kata sandi tidak sesuai']")).getText(),
+                "Kolom ulangi kata sandi tidak sesuai"
+        );
+        Boolean button = driver.findElement(By.cssSelector("button[data-testid='button-submit']")).isEnabled();
+        Assert.assertFalse(button);
+    }
 }
